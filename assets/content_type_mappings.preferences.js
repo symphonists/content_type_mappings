fa669d52a570e468fe0d3dd264e0d86e8d38565d
Nicolas Brassard <nico@nitriques.com>
2011-11-17 23:46:46 -0500

    Code clean up, Liscence and README

--- assets/content_type_mappings.preferences.js
+++ assets/content_type_mappings.preferences.js
@@ -9,17 +9,6 @@ Content Type Mappings
 	      orderable: true,
 	      collapsible: true
 	    });
-	    
-	    /*duplicator.bind('collapsestop', function(event, item) {
-	      var instance = $(item);
-	      instance.find('.header > span:not(:has(i))').append(
-	        $('<i />').text(instance.find('label:first input').attr('value') + " to " + instance.find('label:nth-child(2) input').attr('value'))
-	      );
-	    });
-	    
-	    duplicator.bind('expandstop', function(event, item) {
-	      $(item).find('.header > span > i').remove();
-	    });*/
   };
 	
   $(init);
